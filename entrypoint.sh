f050d9047d0afb9f634ff9cf82c8af4801fbf7f8
Guillaume <guillaume.jacquart@abtasty.com>
2022-02-03 17:22:03 +0100

    Fix separate build folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,10 @@ echo "downloading dependencies"
 go mod download
 
 echo "building linux library"
-GOOS=linux GOARCH=amd64 go build -buildmode=c-shared -o build/libflagship.so
+GOOS=linux GOARCH=amd64 go build -buildmode=c-shared -o build/linux/libflagship.so
 
 echo "building mac library"
-GOOS=darwin GOARCH=amd64 CC=o64-clang CXX=o64-clang++ go build -buildmode=c-shared -o build/libflagship.dylib
+GOOS=darwin GOARCH=amd64 CC=o64-clang CXX=o64-clang++ go build -buildmode=c-shared -o build/darwin/libflagship.dylib
 
 echo "building windows library"
-GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -buildmode=c-shared -o build/libflagship.dll
-
-pwd
-ls -l
-ls -l build
+GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -buildmode=c-shared -o build/windows/libflagship.dll
